0d781fdaa09ca83a8f70bd4d6de58aada9b14e12
Vijay Kumar <kumar.vijay@gmail.com>
2012-05-20 18:03:16 -0700

    Addes CSS Style and fixed some layout

--- src/com/shastram/web8085/client/MainWindow.java
+++ src/com/shastram/web8085/client/MainWindow.java
@@ -1,5 +1,7 @@
 package com.shastram.web8085.client;
 
+import java.util.HashMap;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.i18n.client.NumberFormat;
@@ -8,7 +10,6 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RichTextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -34,38 +35,84 @@ public class MainWindow extends Composite {
     VerticalPanel memoryWindow;
 
     @UiField
+    VerticalPanel memoryWindowAddress;
+
+    @UiField
     Button stepButton;
 
     @UiField
-    ListBox registerWindow;
+    VerticalPanel registerWindowValues;
+    @UiField
+    VerticalPanel registerWindowNames;
 
     private final Exe exe = new Exe();
     private final NumberFormat formatter = NumberFormat.getFormat("0000");
+
+    private HashMap<String, TextBox> registerValueMap;
 
     public MainWindow() {
         initWidget(uiBinder.createAndBindUi(this));
         Style style = new Style(); // create a dummy one
         Style.style = style;
         sourceCode.setHTML("mvi b,2</br>mov a,b</br>mov c,b");
-        refreshRegisters();
         createMemoryWindowItems();
+        createRegisterWindowNames();
+        refreshRegisters();
         refreshMemory();
+        // refresh the register and memory window to remove the red highlight
+        refreshRegisters();
+        refreshMemory();
+    }
+
+    private void createRegisterWindowNames() {
+        registerWindowNames.clear();
+        String[] names = { "a", "b", "c", "d", "e", "h", "l", "sp", "psw", "ip" };
+        registerValueMap = new HashMap<String, TextBox>();
+        for (String n : names) {
+            TextBox name = createValueTextbox();
+            name.setText(n);
+            name.setReadOnly(true);
+            registerWindowNames.add(name);
+            TextBox value = createValueTextbox();
+            registerValueMap.put(n, value);
+            registerWindowValues.add(value);
+        }
     }
 
     private void createMemoryWindowItems() {
         memoryWindow.clear();
-        for (int i = 0; i < 20; ++i) {
-            TextBox tb = new TextBox();
-            tb.addStyleName(Style.style.css.memoryTextBox());
-            memoryWindow.add(tb);
+        for (int i = 0; i < 10; ++i) {
+            TextBox addr = createValueTextbox();
+            addr.setReadOnly(true);
+            memoryWindowAddress.add(addr);
+            TextBox value = createValueTextbox();
+            memoryWindow.add(value);
         }
+    }
+
+    public TextBox createValueTextbox() {
+        TextBox addr = new TextBox();
+        addr.addStyleName(Style.style.css.memoryTextBox());
+        return addr;
     }
 
     private void fillMemoryWindow(int start) {
         for (int i = 0; i < memoryWindow.getWidgetCount(); ++i) {
-            TextBox tb = (TextBox) memoryWindow.getWidget(i);
             int addr = start + i;
-            tb.setText(formatter.format(addr) + ":  " + formatter.format(exe.getMemory(addr)));
+            TextBox addrBox = (TextBox) memoryWindowAddress.getWidget(i);
+            addrBox.setText(" " + formatter.format(addr) + ":  ");
+            TextBox value = (TextBox) memoryWindow.getWidget(i);
+            updateTextboxValue(exe.getMemory(addr), value);
+            /*
+            String newValue = " " + formatter.format(exe.getMemory(addr)) + " ";
+            String oldValue = value.getText();
+            if (!oldValue.equals(newValue) && highlight) {
+                value.addStyleName(Style.style.css.memoryWindowHighlight());
+            } else {
+                value.removeStyleName(Style.style.css.memoryWindowHighlight());
+            }
+            value.setText(newValue);
+            */
         }
     }
 
@@ -77,6 +124,9 @@ public class MainWindow extends Composite {
             exe.reset();
             errorWindow.setText("Finished parsing");
             exe.reset();
+            refreshRegisters();
+            refreshMemory();
+            // refresh the register and memory window to remove the red highlight
             refreshRegisters();
             refreshMemory();
         } catch (Exception ex) {
@@ -101,16 +151,27 @@ public class MainWindow extends Composite {
     }
 
     public void refreshRegisters() {
-        registerWindow.clear();
+        updateTextboxValue(exe.a, registerValueMap.get("a"));
+        updateTextboxValue(exe.b, registerValueMap.get("b"));
+        updateTextboxValue(exe.c, registerValueMap.get("c"));
+        updateTextboxValue(exe.d, registerValueMap.get("d"));
+        updateTextboxValue(exe.e, registerValueMap.get("e"));
+        updateTextboxValue(exe.h, registerValueMap.get("h"));
+        updateTextboxValue(exe.l, registerValueMap.get("l"));
+        updateTextboxValue(exe.l, registerValueMap.get("sp"));
+        updateTextboxValue(exe.l, registerValueMap.get("psw"));
+        updateTextboxValue(exe.ip, registerValueMap.get("ip"));
+    }
 
-        registerWindow.addItem("a = " + toHex(exe.a));
-        registerWindow.addItem("b = " + toHex(exe.b));
-        registerWindow.addItem("c = " + toHex(exe.c));
-        registerWindow.addItem("d = " + toHex(exe.d));
-        registerWindow.addItem("e = " + toHex(exe.e));
-        registerWindow.addItem("h = " + toHex(exe.h));
-        registerWindow.addItem("l = " + toHex(exe.l));
-        registerWindow.addItem("ip = " + toHex(exe.ip));
+    private void updateTextboxValue(int v, TextBox textBox) {
+        String oldValue = textBox.getText().trim();
+        String newValue = toHex(v);
+        textBox.setText(newValue);
+        if (oldValue.equals(newValue)) {
+            textBox.removeStyleName(Style.style.css.memoryWindowHighlight());
+        } else {
+            textBox.addStyleName(Style.style.css.memoryWindowHighlight());
+        }
     }
 
     private String toHex(int i) {
---
+++ src/com/shastram/web8085/client/Style.java
@@ -0,0 +1,34 @@
+package com.shastram.web8085.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class Style extends Composite {
+
+    private static StyleUiBinder uiBinder = GWT.create(StyleUiBinder.class);
+
+    public interface Css extends CssResource {
+        String memoryTextBox();
+
+        String memoryWindowHighlight();
+    }
+
+    @UiField
+    Css css;
+
+    // public static reference
+    public static Style style;
+
+    interface StyleUiBinder extends UiBinder<Widget, Style> {
+    }
+
+    public Style() {
+        initWidget(uiBinder.createAndBindUi(this));
+        Style.style = this;
+    }
+
+}
